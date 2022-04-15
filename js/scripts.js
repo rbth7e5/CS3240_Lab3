0513148676bb1b20718fb9a9b364404fde044885
Caijie <caijie96@gmail.com>
2022-04-16 04:17:52 +0800

    add: redirect resume to gdrive

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
-import booleanContains from "@turf/boolean-contains";
-import { point, polygon } from "@turf/helpers";
 import {
-  sg_geojson,
   navHighlighter,
   dragElement,
   navAnimator,
@@ -9,27 +6,7 @@ import {
 } from "./consts";
 
 window.downloadResume = function () {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const userLoc = point([
-          position.coords.longitude,
-          position.coords.latitude,
-        ]);
-        const sg_polygon = polygon(sg_geojson.features[0].geometry.coordinates);
-        if (booleanContains(sg_polygon, userLoc)) {
-          document.getElementById("download-local").click();
-        } else {
-          document.getElementById("download-resume").click();
-        }
-      },
-      () => {
-        document.getElementById("download-resume").click();
-      }
-    );
-  } else {
-    document.getElementById("download-resume").click();
-  }
+  window.open('https://docs.google.com/document/d/1NPI-piwKe-UpZ0HXgYMwgq9PFD0h3gsX1yV5ZfGd3ko/edit?usp=sharing', '_blank').focus();
 };
 
 document.getElementById("resume-icon").onclick = downloadResume;
