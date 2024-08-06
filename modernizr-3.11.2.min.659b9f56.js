6a098dad5449169a0df35ab9580882de0b46ba9b
Caijie Chen <chen.caijie@thetradedesk.com>
2024-08-07 00:14:19 +0800

    Updates

--- modernizr-3.11.2.min.659b9f56.js
+++ modernizr-3.11.2.min.659b9f56.js
@@ -523,7 +523,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55322" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56914" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
